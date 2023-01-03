f6b2117b32bbdb78e06d29e52aac4bfee51ee61c
lbruand <lbruand@deeppay.net>
2023-01-03 10:12:06 +0100

    cleanup stuff

--- load_mp3.py
+++ load_mp3.py
@@ -8,9 +8,6 @@ import sys
 
 CLEANR = re.compile('<.*?>') 
 
-# def base_url(word, number=1):
-# 	letter = word[0]	
-# 	return f"https://media.merriam-webster.com/audio/prons/en/us/mp3/{letter}/{word.ljust(5, '0')}{number:03}.mp3"
 
 def base_url(word, filename):
 	letter = word[0]	
@@ -33,25 +30,6 @@ def cleanword(word):
 with open("input.json", "r") as fp:
 	rows = json.load(fp,)
 
-# outrows=[]
-# for row in rows:
-# 	french = row["french"]
-# 	infinitive =  row["infinitive"]["text"]
-# 	preterite = row["preterite"]["text"]
-# 	pastparticipe = row["pastparticipe"]["text"]
-# 	filename1 = base_url(cleanword(infinitive), 1).split("/")[-1]
-# 	filename2 = base_url(cleanword(infinitive), 2).split("/")[-1]
-# 	outrows.append({
-# 				"french":french, 
-#  				"infinitive": {"text": infinitive, "mwid":filename1},
-#  				"preterite": {"text": preterite, "mwid":filename2},
-#  				"pastparticipe": {"text": pastparticipe, "mwid":filename2},
-# 	})
-
-# with open("output.json", "w") as fp:
-# 	json.dump(outrows, fp, indent=2)
-
-# sys.exit(0)
 
 TEST_MODEL = genanki.Model(
   234567, 'irregular verbs',
@@ -94,13 +72,11 @@ Path("sounds").mkdir(parents=True, exist_ok=True)
 
 output_words = []
 
-for row in tqdm.tqdm(rows):
-	print(row)
+for row in tqdm.tqdm(rows):	
 	french = row["french"]
 	infinitive =  row["infinitive"]["text"]
 
 	output_row = [french]
-	#for ix, word in enumerate([infinitive, preterite, pastparticipe]):
 	for d in ['infinitive', 'preterite', 'pastparticipe']:
 		word = row[d]['text']
 		ix = row[d]['mwid']
@@ -116,12 +92,12 @@ for row in tqdm.tqdm(rows):
 			with open(output_file, "wb") as fp:
 				fp.write(response.content)
 			media_files.append(output_file)
-		print(word)
+
 		output_row.append(f"{word} [sound:{output_filename}]")
 	output_words.append(output_row)
 
 
-print(media_files)
+
 for note_word in output_words:
 	note = genanki.Note(TEST_MODEL, note_word)
 	deck.add_note(note)
